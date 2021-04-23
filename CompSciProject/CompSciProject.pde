e8bd4afe56898224ee5ad18a25881f2de060845e
gabfoil <79896421+gabfoil@users.noreply.github.com>
2021-04-23 16:13:40 -0400

    edits

---
+++ CompSciProject/CompSciProject.pde
@@ -0,0 +1,13 @@
+import interfascia.*;
+
+
+void setup() {
+    size(400,400);
+    background(150);
+}
+
+void draw() {
+    background(192, 64, 0);
+    stroke(255,255,0);
+    line(150, 25, mouseX, mouseY);
+}
--- Game.java
+++ Game.java
@@ -1,5 +1,11 @@
+import Interfascia;
+
 public class Game {
-    public void main(String[] args) {
-        
+    public static void main(String[] args) {
+        //setup();
     }
+    void setup() {
+        size(400, 400);
+        
+      }
 }
